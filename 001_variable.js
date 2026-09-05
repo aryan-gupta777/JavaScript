8d6ea7df11220b5f8952f559c298e89c22925130
aryan <aryangupta562006@gmail.com>
2026-09-06 01:48:54 +0530

    js

---
+++ 001_variable.js
@@ -0,0 +1,9 @@
+const accountId=1425
+let accountEmail="aryan123@gmail.com"
+var accountPassword ="a2345"
+
+accountCity = "noida"
+let accountState
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 002_datatypes.js
@@ -0,0 +1,9 @@
+"use strict" // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(typeof "aryan")
+console.log(typeof null)
+// null => standalone value  , it is an object
+
+console.log(typeof undefined)
