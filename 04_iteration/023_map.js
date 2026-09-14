424a9d094d9a8f57b89df94fcf685272f3c92a4e
aryan <aryangupta562006@gmail.com>
2026-09-14 23:50:52 +0530

    map

---
+++ 04_iteration/023_map.js
@@ -0,0 +1,45 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+
+const arr =[1,2,3,4,5]
+
+for (const i of arr) {
+    // console.log(i);  // 1,2,3,4,5
+}
+
+const greeting ="! hello"
+for (const i of greeting) {
+    // console.log(i)
+}
+
+// --------------------map-----------------------------
+
+const m =new Map()
+
+m.set("os","operating system")
+m.set("net","networking")
+m.set("dbms","data base managment system")
+m.set("oops","object oriented pragramming")
+
+// console.log(m.keys()); //  {'os', 'net', 'dbms', 'oops' }
+// console.log(m.values());//  {d 'operating system', 'networking', 'data base managment system','object oriented pragramming' }
+// console.log(m);  // both printed
+
+// console.log(m.get("os"));  // get returns the value of the key searched
+
+// for (const [key,value] of m) {
+//     console.log(key ,":-",value);
+// }
+
+const myObject={
+    game1:'nfs',
+    game2:'spiderman'
+}
+
+for (const [key,value] of Object.entries(myObject)) {
+    console.log(key ,":-",value);
+}
+
